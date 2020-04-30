1e152771ad4414d023815306fc01f065454e52e1
Tomoya-Sonok <wlcmty08kh@gmail.com>
2020-04-30 16:09:38 +0900

    Add some files

---
+++ paiza.rb
@@ -0,0 +1,15 @@
+s = 2
+array3= []
+#   n = 1/1.to_f
+#   p n
+  some_num = Rational(1, s)
+  p some_num
+  el = some_num.to_f
+  p el
+#   p some_num.to_f
+array3 << el
+#   p n + some_num
+array_sum = array3.sum
+p array3.sum
+  s += 1
+  p "----------------"
